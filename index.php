57a4b773ae49081277b52e28c4575e7bcb8f836e
Sunil kumar <sunilkumar3020@gmail.com>
2025-09-18 17:20:42 +0530

    update file

--- index.php
+++ index.php
@@ -5,12 +5,3 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project </title>
-</head>
-
-<body>
-
-    <h2>Project test</h2>
-
-</body>
-
-</html>
